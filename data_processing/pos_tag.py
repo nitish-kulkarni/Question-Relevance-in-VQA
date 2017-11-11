0bdffcfdecf8f2ec3dc2e3c1d492ea3cd1d29540
Nitish Kulkarni <ni4lgi@gmail.com>
2017-11-10 20:34:37 -0500

    Expose postags method.

--- data_processing/pos_tag.py
+++ data_processing/pos_tag.py
@@ -3,11 +3,17 @@ import sys
 
 nlp = spacy.load('en')
 
-for line in sys.stdin:
-	line = line.strip("\n")
-	(label,question) = line.split("\t")
-	parsed_question = nlp(question.decode('utf-8'))
-	output_string = ""
-	for token in parsed_question:
-		output_string += token.text + "|" + token.pos_ + "|"
-	sys.stdout.write(label + "\t" + output_string[:-1].encode('utf-8').strip() + "\n")
+def postags(question):
+    return [(token.text, token.pos_) for token in nlp(question.decode('utf-8'))]
+
+def main():
+	for line in sys.stdin:
+		line = line.strip("\n")
+		label, question = line.split("\t")
+		output_string = ''
+		for word_pos in postags(question):
+			output_string += '%s|%s|' % word_pos
+		sys.stdout.write(label + "\t" + output_string[:-1].encode('utf-8').strip() + "\n")
+
+if __name__ == '__main__':
+	main()
