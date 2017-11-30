52aec6ebf04aabdc35063eb465c2dd25b7a1245a
Nitish Kulkarni <ni4lgi@gmail.com>
2017-11-30 06:23:05 -0500

    Spice IO files, json manipulations

---
+++ data_generation/premise/extract_premise/run_spice/split_jsons.py
@@ -0,0 +1,20 @@
+import json
+
+INPUT_FILE = 'small_spice_input.json'
+OUTPUT_PREFIX = 'spice_input_file'
+NUM_SPLITS = 10
+
+def main():
+    with open(INPUT_FILE, 'r') as fp:
+        data = json.load(fp)
+    split_size = len(data) // NUM_SPLITS
+
+    print "Data Size = %d\nSplit Size=%d" % (len(data), split_size)
+
+    for i in range(NUM_SPLITS):
+        values = data[i * split_size: (i+1) * split_size]
+        with open('%s_%d.json' % (OUTPUT_PREFIX, i), 'w') as fp:
+            json.dump(values, fp)
+
+if __name__ == '__main__':
+    main()
