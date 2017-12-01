7f937ab37971da5c50b6f154fa5f553222c4710b
Nitish Kulkarni <ni4lgi@gmail.com>
2017-12-01 02:29:37 -0500

    Changes to include vg features

--- data_processing/get_visual_features.py
+++ data_processing/get_visual_features.py
@@ -1,37 +1,56 @@
 import torchfile
-### To install torchfile, do pip install torchfile
 import json
+import numpy as np
 
 ### Collecting necessary image ids for extracting features
 unique_image_ids = {}
-data_train = json.load(open("../../project_nongit/Data/train.json","r"))
-data_test = json.load(open("../../project_nongit/Data/test.json","r"))
+base_path = '../data/true_vs_false_premise'
+data_train = json.load(open("%s/train.json" % base_path,"r"))
+data_test = json.load(open("%s/test.json" % base_path,"r"))
 n_train = len(data_train)
 n_test = len(data_test)
 
-for i in range(n_train):
-	n_id_pairs = len(data_train[i]["tuplist"])
-	for j in range(n_id_pairs):
-		unique_image_ids[data_train[i]["tuplist"][j]["irr_imid"]] = 1
-		unique_image_ids[data_train[i]["tuplist"][j]["rel_imid"]] = 1
-for i in range(n_test):
-	n_id_pairs = len(data_test[i]["tuplist"])
-	for j in range(n_id_pairs):
-		unique_image_ids[data_test[i]["tuplist"][j]["irr_imid"]] = 1
-		unique_image_ids[data_test[i]["tuplist"][j]["rel_imid"]] = 1
+### Collecting fc7 features for images
+base_path = '../data_generation/premise/generate_IQ_pairs/img_data'
+coco_train_feat = torchfile.load("%s/train_fc7.t7" % base_path)
+coco_val_feat = torchfile.load("%s/val_fc7.t7" % base_path)
+vg_feat = torchfile.load("%s/vg_fc7.t7" % base_path)
+coco_train_dict = json.load(open("%s/coco_train_dict.json" % base_path, "r"))
+coco_val_dict = json.load(open("%s/coco_val_dict.json" % base_path, "r"))
+vg_dict = json.load(open("%s/vg_dict.json" % base_path, "r"))
 
-print "Number of unique image ids in all of train and test", len(unique_image_ids)
+# Data output path
+output_path = '../data/true_vs_false_premise'
 
-coco_train_feat = torchfile.load("../../project_nongit/Data/train_fc7.t7")
-coco_val_feat = torchfile.load("../../project_nongit/Data/val_fc7.t7")
-coco_train_dict = json.load(open("../../project_nongit/Data/coco_train_dict.json", "r"))
-coco_val_dict = json.load(open("../../project_nongit/Data/coco_val_dict.json", "r"))
+def get_fc7(imid):
+	if imid in coco_train_dict:
+		return coco_train_feat[coco_train_dict[imid]]
 
-no_imgfeat_ids = {}
-for img_id in unique_image_ids:
-	if str(img_id) in coco_train_dict:
-		unique_image_ids[img_id] = coco_train_feat[coco_train_dict[str(img_id)]]
-	elif str(img_id) in coco_val_dict:
-		unique_image_ids[img_id] = coco_val_feat[coco_val_dict[str(img_id)]]
-	else:
-		no_imgfeat_ids[img_id] = 1
+	if imid in coco_val_dict:
+		return coco_train_feat[coco_val_dict[imid]]
+
+	if imid in vg_dict:
+		return vg_feat[vg_dict[imid]]
+
+	raise 'Unknown image id ', imid
+
+def get_feature(imid, question, relevance):
+    return {
+        'question': question,
+        'image_id': imid,
+        'image_features_fc7': str(list(get_fc7(str(imid)))),
+        'relevance': relevance
+    }
+
+data = []
+for d in data_train + data_test:
+    for val in d['tuplist']:
+        data.append(get_feature(val['rel_imid'], d['q'], True))
+        data.append(get_feature(val['irr_imid'], d['q'], False))
+
+np.random.shuffle(data)
+
+print 'Saving %d Q-I pairs' % len(data)
+with open('%s/data.json' % output_path, 'w') as fp:
+    json.dump(data, fp)
+
