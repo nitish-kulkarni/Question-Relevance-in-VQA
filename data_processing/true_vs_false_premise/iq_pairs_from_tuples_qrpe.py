736196430d8eb5cc02a31653f1ee1f48bea97790
Nitish Kulkarni <ni4lgi@gmail.com>
2017-12-02 11:14:51 -0500

    Processing file for qrpe and extended dataset

---
+++ data_processing/true_vs_false_premise/iq_pairs_from_tuples_qrpe.py
@@ -0,0 +1,34 @@
+"""Processes raw tuples and creates annotated question image pairs
+"""
+
+import sys
+import json
+import numpy as np
+
+def get_tuple(qid, imid, relevance, source):
+    return '%s\t%s\t%d\t%d' % (imid, qid, relevance, source)
+
+def main():
+    input_filename, order = sys.argv[1:]
+    order = int(order)
+    assert order in [1, 2]
+    
+    with open(input_filename, 'r') as fp:
+        data = json.load(fp)
+    
+    tuples = set()
+    for d in data:
+        qid = d['qid']
+        for tup in d['tuplist']:
+            rel_id = tup['rel_imid']
+            irr_id = tup['irr_imid']
+            tuples.add(get_tuple(str(rel_id), str(qid), True, True))
+            tuples.add(get_tuple(str(irr_id), str(qid), False, len(tup['irr_tuple']) == 0))
+
+    tuples = list(tuples)
+    idxs = np.random.permutation(len(tuples))
+    for i in idxs:
+        print(tuples[i])
+
+if __name__ == '__main__':
+    main()
