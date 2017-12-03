c6979b23a9d8531569c013edc47b69c3104dc24c
Prakruthi Prabhakar <prakruthi@Prakruthis-MBP.wv.cc.cmu.edu>
2017-12-02 23:58:39 -0500

    Adding PCA for new features and new LSTM model

--- data_processing/true_vs_false_premise/features.py
+++ data_processing/true_vs_false_premise/features.py
@@ -4,8 +4,11 @@
 import json
 import torchfile
 import numpy as np
+from sklearn.decomposition import PCA
 
-DATA_PATH = '../../data/true_vs_false_premise/extended/processed'
+np.random.seed(42)
+
+DATA_PATH = "../../../project_nongit/Data/"
 
 # Question and image maps
 QUESTIONS_MAP = json.load(open(DATA_PATH + '/vqa2_questions.json', 'r'))
@@ -15,10 +18,12 @@ COCO_VAL_IMG_MAP = json.load(open("%s/coco_val_image_map.json" % DATA_PATH, "r")
 VG_IMG_MAP = json.load(open("%s/vg_image_map.json" % DATA_PATH, "r"))
 
 ### Collecting fc7 features for images
-# DATA_PATH = '/Volumes/Nitish-Passport/10605_project_files/data_generation/premise/generate_IQ_pairs/img_data'
 COCO_TRAIN_FEAT = torchfile.load("%s/train_fc7.t7" % DATA_PATH)
 COCO_VAL_FEAT = torchfile.load("%s/val_fc7.t7" % DATA_PATH)
 VG_FEAT = torchfile.load("%s/vg_fc7.t7" % DATA_PATH)
+
+TRAIN_PATH = DATA_PATH + "train_firstorder_data.txt"
+VAL_PATH = DATA_PATH + "val_firstorder_data.txt"
 
 def get_image_feature(image_id, coco=True):
     """inputs: 
@@ -53,4 +58,76 @@ def get_questions(question_ids):
     outputs: Corresponding VQA questions as list of strings
     """
     return [get_question(qid) for qid in question_ids]
+
+### Computing PCA features
+uniq_img_ids = {}
+with open(TRAIN_PATH, "r") as f:
+    for line in f:
+        line = line.strip("\n")
+        arr = line.split("\t")
+        uniq_img_ids[arr[0]] = arr[3]
+with open(VAL_PATH, "r") as f:
+    for line in f:
+        line = line.strip("\n")
+        arr = line.split("\t")
+        uniq_img_ids[arr[0]] = arr[3]
+image_ids = []
+coco = []
+for key in uniq_img_ids:
+    image_ids.append(key)
+    coco.append(uniq_img_ids[key])
+
+img_features = get_image_features(image_ids, coco)
+pca = PCA(n_components=300)
+img_transformed_features = pca.fit_transform(img_features)
+
+f = open(TRAIN_PATH, "r")
+train_lines = f.readlines()
+f.close()
+
+### Creating train and validation data
+train_data_split = 0.8
+total_data_instances = len(train_lines)
+perm = np.random.permutation(range(total_data_instances))
+train_end_idx = int(train_data_split*total_data_instances)
+train_data = [train_lines[i] for i in perm[0:train_end_idx]]
+val_data = [train_lines[i] for i in perm[train_end_idx:total_data_instances]]
+
+fid = open("processed_train_data.txt", "w")
+for line in train_data:
+    line = line.strip("\n")
+    (img_id, qid, rel, src) = line.split("\t")
+    img_fts = ",".join(["{:.4f}".format(x) for x in img_transformed_features[image_ids.index(img_id)]])
+    ques = get_question(qid)
+    fid.write(img_fts + "\t" + ques + "\t" + str(rel) + "\n")
+fid.close()
+
+fid = open("processed_val_data.txt", "w")
+for line in val_data:
+    line = line.strip("\n")
+    (img_id, qid, rel, src) = line.split("\t")
+    img_fts = ",".join(["{:.4f}".format(x) for x in img_transformed_features[image_ids.index(img_id)]])
+    ques = get_question(qid)
+    fid.write(img_fts + "\t" + ques + "\t" + str(rel) + "\n")
+fid.close()
+
+### Creating test data
+f = open(VAL_PATH, "r")
+test_lines = f.readlines()
+f.close()
+
+fid = open("processed_test_data.txt", "w")
+for line in test_lines:
+    line = line.strip("\n")
+    (img_id, qid, rel, src) = line.split("\t")
+    img_fts = ",".join(["{:.4f}".format(x) for x in img_transformed_features[image_ids.index(img_id)]])
+    ques = get_question(qid)
+    fid.write(img_fts + "\t" + ques + "\t" + str(rel) + "\n")
+fid.close()
+
+
+
+
+
+
 
--- model/true_false_premise/lstm.py
+++ model/true_false_premise/lstm.py
@@ -9,47 +9,48 @@ from keras.regularizers import l2
 from keras.callbacks import ModelCheckpoint
 from sklearn.metrics import precision_recall_fscore_support as score
 import json
-import ast
 
 EMBEDDING_LEN = 300
 
 class Dataset():
 	tokenizer = None
-	data = None
+	data_train = None
+	data_val = None
+	data_test = None
 	word_to_idx = None
 	total_words = None
-	num_imgs = None
+	num_imgs_train = None
+	num_imgs_val = None
+	num_imgs_test = None
 
 	def __init__(self, datapath):
 		self.tokenizer = text.Tokenizer()
-		self.data = pd.read_json(datapath)
-		self.data.question = self.data.question.astype(str)
-		self.tokenizer.fit_on_texts(list(self.data.question))
+		self.data_train = pd.read_csv(datapath + "processed_train_data.txt", sep="\t", header=None)
+		self.data_train.columns = ["img_fts","ques","rel"]
+		self.data_val = pd.read_csv(datapath + "processed_val_data.txt", sep="\t", header=None)
+		self.data_val.columns = ["img_fts","ques","rel"]
+		self.data_test = pd.read_csv(datapath + "processed_test_data.txt", sep="\t", header=None)
+		self.data_test.columns = ["img_fts","ques","rel"]
+		self.data_train.ques = self.data_train.ques.astype(str)
+		self.data_val.ques = self.data_val.ques.astype(str)
+		self.data_test.ques = self.data_test.ques.astype(str)
+		self.tokenizer.fit_on_texts(list(self.data_train.ques) + list(self.data_val.ques) + list(self.data_test.ques))
 		self.word_to_idx = self.tokenizer.word_index
 		self.total_words = len(self.word_to_idx)
-		self.num_imgs = len(self.data.image_id)
-		idx = self.total_words + 1
-		new_idx = np.expand_dims((np.array(range(1,self.num_imgs+1))+idx).T,axis=1)
-		self.data.image_id = new_idx
+		self.num_imgs_train = len(self.data_train.img_fts)
+		self.num_imgs_val = len(self.data_val.img_fts)
+		self.num_imgs_test = len(self.data_test.img_fts)
 
-	def create_dataset(self, train_data_split):
-		total_data_instances = len(self.data)
-		# Shuffle the data indexes
-		np.random.seed(42)
-		perm = np.random.permutation(self.data.index)
-		train_end_idx = int(train_data_split*total_data_instances)
-		# Create train and test based on input split
-		train_data = self.data.iloc[perm[0:train_end_idx]]
-		test_data = self.data.iloc[perm[train_end_idx:total_data_instances]]
-		print "Number of train data instances read", len(train_data)
-		print "Number of test data instances read", len(test_data)
-		return train_data, test_data
-
-	def process_dataframe(self, inpdata, max_len_sentence):
-		X = self.tokenizer.texts_to_sequences(inpdata.question)
+	def process_dataframe(self, inpdata, max_len_sentence, flag):
+		X = self.tokenizer.texts_to_sequences(inpdata.ques)
 		X = sequence.pad_sequences(X, maxlen=max_len_sentence)
-		X = np.concatenate((np.expand_dims(inpdata.image_id,1),X),axis=1)
-		Y = inpdata.relevance.astype(int)
+		if flag==0:
+			X = np.concatenate((np.expand_dims(range(self.total_words+1,self.total_words+1+self.num_imgs_train),1),X),axis=1)
+		elif flag==1:
+			X = np.concatenate((np.expand_dims(range(self.total_words+1+self.num_imgs_train,self.total_words+1+self.num_imgs_train+self.num_imgs_val),1),X),axis=1)
+		else:
+			X = np.concatenate((np.expand_dims(range(self.total_words+1+self.num_imgs_train+self.num_imgs_val,self.total_words+1+self.num_imgs_train+self.num_imgs_val+self.num_imgs_val),1),X),axis=1)
+		Y = inpdata.rel.astype(int)
 		return X,Y
 
 	def create_embedding_matrix(self, embeddings_path):
@@ -59,20 +60,24 @@ class Dataset():
 				values = line.split()
 				embedding = np.asarray(values[1:], dtype='float32')
 				embeddings[values[0]] = embedding
-		sz_embedding_mat = self.total_words + 1 + self.num_imgs
+		sz_embedding_mat = self.total_words + 1 + self.num_imgs_train + self.num_imgs_val + self.num_imgs_test
 		embedding_matrix = np.zeros((sz_embedding_mat, EMBEDDING_LEN))
 		for key in self.word_to_idx:
 			if key in embeddings:
 				embedding_matrix[self.word_to_idx[key]] = embeddings[key]
-		for i in range(self.total_words+1, sz_embedding_mat):
-			embedding_matrix[i] = np.array(ast.literal_eval(self.data.image_features_fc7[i-self.total_words-1]))
+		print "Initialized word embeddings"
+		embedding_matrix[self.total_words+1:self.total_words+1+self.num_imgs_train] = np.array([np.fromstring(key, dtype=np.float16, sep=",").reshape(300) for key in self.data_train.img_fts])
+		print "Initialized train features as embeddings"
+		embedding_matrix[self.total_words+1+self.num_imgs_train:self.total_words+1+self.num_imgs_train+self.num_imgs_val] = np.array([np.fromstring(key, dtype=np.float16, sep=",").reshape(300) for key in self.data_val.img_fts])
+		print "Initialized val features as embeddings"
+		embedding_matrix[self.total_words+1+self.num_imgs_train+self.num_imgs_val:self.total_words+1+self.num_imgs_train+self.num_imgs_val+self.num_imgs_test] = np.array([np.fromstring(key, dtype=np.float16, sep=",").reshape(300) for key in self.data_test.img_fts])
+		print "Initialized test features as embeddings"
 		return embedding_matrix
 
 class LSTMModel():
 	def build_model(self, num_vocab, embedding_matrix, max_len):
 		lstm = Sequential()
-		lstm.add(Embedding(input_dim=num_vocab, output_dim=EMBEDDING_LEN, \
-			weights=[embedding_matrix], input_length=max_len, trainable=False))
+		lstm.add(Embedding(input_dim=num_vocab, output_dim=EMBEDDING_LEN, weights=[embedding_matrix], input_length=max_len, trainable=False))
 		lstm.add(LSTM(256, dropout_W=0.5, dropout_U=0.5))
 		lstm.add(Dense(100, activation='relu', W_regularizer=l2(0.0001), b_regularizer=l2(0.0001)))
 		lstm.add(Dense(50, activation='relu', W_regularizer=l2(0.0001), b_regularizer=l2(0.0001)))
@@ -93,14 +98,12 @@ def main(params):
 	model_path = params["model_path"]
 
 	Ds = Dataset(datapath)
-	train_data, test_data = Ds.create_dataset(train_data_split)
-	X_train, Y_train = Ds.process_dataframe(train_data, max_len_sentence)
-	# Storage reduction
-	train_data = None
-	print "Obtained processed training data"
+	X_train, Y_train = Ds.process_dataframe(Ds.data_train, max_len_sentence, 0)
+	X_val, Y_val = Ds.process_dataframe(Ds.data_val, max_len_sentence, 1)
+	print "Obtained processed training and validation data"
 	embedding_matrix = Ds.create_embedding_matrix(embeddings_path)
 	print "Obtained embeddings"
-	num_vocab = Ds.total_words + 1 + Ds.num_imgs
+	num_vocab = Ds.total_words + 1 + Ds.num_imgs_train + Ds.num_imgs_val + Ds.num_imgs_test
 
 	lm = LSTMModel()
 	model = lm.build_model(num_vocab, embedding_matrix, max_len_sentence+1)
@@ -109,9 +112,9 @@ def main(params):
 	filepath = model_path + "weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
 	checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
 	callbacks_list = [checkpoint]
-	model.fit(x=[X_train], y=Y_train, batch_size=128, epochs=30, verbose=1, validation_split=0.2, shuffle=True, callbacks=callbacks_list)
+	model.fit(x=[X_train], y=Y_train, batch_size=128, epochs=30, verbose=1, shuffle=True, callbacks=callbacks_list, validation_data=(X_val, Y_val))
 
-	X_test, Y_test = Ds.process_dataframe(test_data, max_len_sentence)
+	X_test, Y_test = Ds.process_dataframe(test_data, max_len_sentence, 2)
 	pred = model.predict(X_test, batch_size=32, verbose=0)
 	precision, recall, fscore, support = score(Y_test, pred.round(), labels=[0, 1])
 
@@ -124,11 +127,10 @@ def main(params):
 if __name__=='__main__':
 	### Read user inputs
 	parser = argparse.ArgumentParser()
-	#parser.add_argument("--datapath", dest="datapath", type=str, default="../../Data/quora_duplicate_questions.tsv")
-	parser.add_argument("--datapath", dest="datapath", type=str, default="data_transformed.json")
+	parser.add_argument("--datapath", dest="datapath", type=str, default="../../../project_nongit/Data/")
 	parser.add_argument("--train_data_split", dest="train_data_split", type=float, default=0.8)
 	parser.add_argument("--max_len_sentence", dest="max_len_sentence", type=int, default=10)
-	parser.add_argument("--embeddings_path", dest="embeddings_path", type=str, default="glove.840B.300d.txt")
+	parser.add_argument("--embeddings_path", dest="embeddings_path", type=str, default="../../../../11-785/Project/Data/glove.840B.300d.txt")
 	parser.add_argument("--model_path", dest="model_path", type=str, default="./models/")
 	params = vars(parser.parse_args())
 	main(params)
