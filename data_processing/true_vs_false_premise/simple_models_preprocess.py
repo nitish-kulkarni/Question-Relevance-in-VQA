6175f30fd0669ce9de639ad9673350ae1babbbdf
Linghao Zhang <linghaozh@gmail.com>
2017-12-03 21:47:00 -0800

    process & do streaming LR for both qrpe & extended datasets

--- data_processing/true_vs_false_premise/simple_models_preprocess.py
+++ data_processing/true_vs_false_premise/simple_models_preprocess.py
@@ -53,7 +53,50 @@ def process_data(mode):
     np.save(DATA_PATH+'y_%s' % mode, labels)
 
 
+def process_qrpe_data(mode):
+    assert mode in ['train', 'test']
+    
+    f = open(DATA_PATH+'processed_qrpe_%s_data.txt' % mode)
+    g = open(DATA_PATH+'qrpe_%s_questions.raw' % mode, 'w')
+    labels = []
+    i = 0
+    for line in f:
+        img_feat, question, label = line.strip().split('\t')
+        g.write(question+'\n')
+        label = int(label)
+        labels.append(label)
+        i += 1
+    g.close()
+
+    cmd = '/home/linghao/fastText-0.1.0/fasttext print-sentence-vectors %s < %s > %s' % \
+            (DATA_PATH+'fastText_model.bin', DATA_PATH+('qrpe_%s_questions.raw'%mode), DATA_PATH+('qrpe_%s_questions.vec'%mode))
+    subprocess.call(cmd, shell=True)
+
+    n = len(labels)
+
+    f = open(DATA_PATH+'processed_qrpe_%s_data.txt' % mode)
+    g = open(DATA_PATH+'qrpe_%s_questions.vec' % mode)
+    h = open(DATA_PATH+'qrpe_Xy_%s.txt' % mode, 'w')
+
+    for i in range(n):
+        if i % 50000 == 0: print(i)
+        line = f.readline()
+        img_feat, _, __ = line.strip().split('\t')
+        img_feat = ast.literal_eval('[%s]'%img_feat)
+        line = g.readline()
+        txt_feat = [float(x) for x in line.strip().split(' ')[-DIM_TXT_FEAT:]]
+        vec = img_feat + txt_feat + [labels[i]]
+        h.write(str(vec)+'\n')
+
+    h.close()
+
+    labels = np.array(labels)
+    np.save(DATA_PATH+'qrpe_y_%s' % mode, labels)
+
+
 if __name__ == '__main__':
 	process_data('train')
 	process_data('val')
 	process_data('test')
+	process_qrpe_data('train')
+	process_qrpe_data('test')
--- model/true_false_premise/LR.py
+++ model/true_false_premise/LR.py
@@ -6,7 +6,6 @@ DATA_PATH = '/home/linghao/Question-Relevance-in-VQA/data/true_vs_false_premise/
 OVERFLOW = 20
 
 
-
 def sigmoid(score):
 	score = min(score, OVERFLOW)
 	score = max(score, -OVERFLOW)
@@ -14,35 +13,63 @@ def sigmoid(score):
 	return exp / (1 + exp)
 
 
-
-def train(train_file, feat_dim=400, learning_rate=0.5, reg_coef=0.0001, verbose=False):
+def train(train_file, val_file, name='first order train dataset', feat_dim=400, learning_rate=0.5, reg_coef=0.0001, max_iters=1, verbose=False):
 	start_time = time.time()
 	weights = np.zeros(feat_dim)
 	decay = 1 - 2 * learning_rate * reg_coef
-	i = 0
-	f = open(train_file)
 
-	for line in f:
-		vec = ast.literal_eval(line.strip())
-		feat = np.array(vec[:feat_dim])
-		label = vec[-1]
+	k = 0
+	while True:
+		f = open(train_file)
+		i = 0
+		total = 0
+		tp, tn, fp, fn = 0, 0, 0, 0
 
-		score = np.dot(weights, feat)
-		p = sigmoid(score)
-		delta = feat * learning_rate * (label - p)
-		weights = weights * decay + delta
+		for line in f:
+			vec = ast.literal_eval(line.strip())
+			feat = np.array(vec[:feat_dim])
+			label = vec[-1]
 
-		i += 1
-		if verbose and i % 50000 == 0:
-			print('%d lines processed, time elapsed = %.2fs' % (i, time.time()-start_time))
+			score = np.dot(weights, feat)
+			p = sigmoid(score)
 
-	return weights, time.time()-start_time
+			total += 1
+			if (label == 1 and p >= 0.5): tp += 1
+			if (label == 0 and p < 0.5): tn += 1
+			if (label == 0 and p >= 0.5): fp += 1
+			if (label == 1 and p < 0.5): fn += 1
+
+			delta = feat * learning_rate * (label - p)
+			weights = weights * decay + delta
+
+			i += 1
+			if verbose and i % 10000 == 0:
+				print('%d lines processed, time elapsed = %.2fs' % (i, time.time()-start_time))
+
+		report(name, tp, tn, fp, fn)
+
+		val_acc = test(val_file, weights)
+		print('validation accuracy = %.2f\n' % val_acc)
+
+		np.save(DATA_PATH+('LR_%d.model'%k), weights)
+
+		k += 1
+		if k == max_iters:
+			break
+		decay = 1 - 2 * learning_rate * reg_coef / k / k
+
+	avg_time = (time.time()-start_time) / max_iters
+	print('=== Average training time per epoch = %.2fs ===\n' % avg_time)
+
+	return weights
 
 
-def test(test_file, weights):
+def test(test_file, weights, name=None, feat_dim=400, verbose=False):
+	start_time = time.time()
 	total = 0
 	tp, tn, fp, fn = 0, 0, 0, 0
 	f = open(test_file)
+	i = 0
 
 	for line in f:
 		vec = ast.literal_eval(line.strip())
@@ -58,16 +85,58 @@ def test(test_file, weights):
 		if (label == 0 and p >= 0.5): fp += 1
 		if (label == 1 and p < 0.5): fn += 1
 
-	return float(tp + tn) / total, (tp, tn, fp, fn)
+		i += 1
+		if verbose and i % 10000 == 0:
+			print('%d lines processed, time elapsed = %.2fs' % (i, time.time()-start_time))
+
+	if name is not None:
+		report(name, tp, tn, fp, fn)
+	return (tp + tn) / total
+
+
+def report(name, tp, tn, fp, fn):
+	print('=== Metrics on %s ===\n' % name)
+
+	total = tp + tn + fp + fn
+	accuracy = (tp + tn) / total
+	precision_true = tp / (tp + fp)
+	precision_false = tn / (tn + fn)
+	recall_true = tp / (tp + fn)
+	recall_false = tn / (tn + fp)
+	fscore_true = (precision_true + recall_true) / 2
+	fscore_false = (precision_false + recall_false) / 2
+	support_true = tp + fn
+	support_false = tn + fp
+
+	print('Classification Report')
+	headers = ['precision', 'recall', 'f1-score','support']
+	head_fmt = u'{:>{width}s} ' + u' {:>9}' * len(headers) + '\n'
+	ret = head_fmt.format(u'', *headers, width=8)
+	row_fmt = u'{:>{width}s} ' + u' {:>9.{digits}f}' * 3 + u' {:>9}\n'
+	ret += row_fmt.format('False', precision_false, recall_false, fscore_false, support_false, width=8, digits=2)
+	ret += row_fmt.format('True', precision_true, recall_true, fscore_true, support_true, width=8, digits=2)
+	print(ret)
+	print('Accuracy = %.2f' % accuracy)
+
+	print('Confusion Matrix')
+	print('%d\t\t%d\n%d\t\t%d\n' % (tn, fp, fn, tp))
+
+
+def main():
+	train_file = DATA_PATH+'Xy_train.txt'
+	val_file = DATA_PATH+'Xy_val.txt'
+	test_file = DATA_PATH+'Xy_test.txt'
+	qrpe_train_file = DATA_PATH+'qrpe_Xy_train.txt'
+	qrpe_test_file = DATA_PATH+'qrpe_Xy_test.txt'
+	
+	weights = train(train_file=train_file, val_file=val_file, max_iters=5, verbose=True)
+	
+	# weights = np.load(DATA_PATH+'LR.model.npy')
+	# test(val_file, weights, name='first order val dataset', verbose=True)
+	# test(test_file, weights, name='first order test dataset', verbose=True)
+	# test(qrpe_train_file, weights, name='qrpe train dataset', verbose=True)
+	# test(qrpe_test_file, weights, name='qrpe test dataset', verbose=True)
 
 
 if __name__ == '__main__':
-	train_file = DATA_PATH + 'Xy_train.txt'
-	test_file = DATA_PATH + 'Xy_test.txt'
-
-	weights, time = train(train_file, verbose=True)
-	np.save('./LR_model', weights)
-	acc, stats = test(test_file, weights)
-
-	print('Accuracy = %.4f, Time = %.2fs' % (acc, time))
-	print(stats)
+	main()
